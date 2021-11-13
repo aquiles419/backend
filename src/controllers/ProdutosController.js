91534664dbc1fb8f0177b5b9b435ba44d3fb59fe
Aquiles <wesley419@gmail.com>
2021-11-13 19:13:37 -0300

    TESTE

--- src/controllers/ProdutosController.js
+++ src/controllers/ProdutosController.js
@@ -25,17 +25,29 @@ class ProdutosController {
     }
 
     list(req, res) {
-        const ean = '9,99E+12';
-        database.select('*').table('produtos').where({
-            ean : ean
-        }).then(data => {
-            console.log(data);
-            res.json(data);
-        }).catch(error => {
-            console.log(error);
-        });
-    }
 
+        const offset = parseInt(req.query.start) || 0
+        const pageSize = req.query.length 
+        database.count('* as count').from("produtos").first().then(count => {
+            console.log("pas",count.count / pageSize);
+            const total = Math.floor(count.count / pageSize)
+        
+            database.select('*').table('produtos').where('DESCRICAO', 'like', `%${req.query.search.value}%`)
+            .offset(offset * pageSize).limit(pageSize).then(data => {
+    
+                res.json({data, draw: req.query.draw , recordsTotal: total , recordsFiltered:total});
+            }).catch(error => {
+                console.log(error);
+            });
+        })
+
+
+
+
+
+
+    }
+    
     getById(req, res) {
         const id = req.params;
         database.select('*').table('produtos').where({
