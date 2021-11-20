93717848b553e895f8b26f3ac4f436df9be9ba9a
Lucas Maciel Vieira <lucas.maciel.vieira@gmail.com>
2021-11-20 10:34:03 -0300

    added http server module

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const http  = require('http');
 
 const router = require('./src/routes/routes');
 
@@ -8,10 +9,25 @@ app.use(cors());
 app.use(express.json());
 app.use(router);
 
-app.listen(4000, () => {
-    console.log('Rodando na porta 4000');
+
+const port        = process.env.PORT || 8080;
+const host        = process.env.HOSTNAME || '0.0.0.0';
+const httpServer  = http.Server(app);
+
+let listeningCallback;
+// Launch Node.js server
+httpServer.listen(port, host, () => {
+    console.log(`Node.js API server is listening on http://${host}:${port}/`);
+    if (listeningCallback) {
+        listeningCallback();
+    }
 });
 
-app.get('/', (request, response) => {
-    response.send("Hello world");
-});
+
+httpServer.init = function (cb) {
+    if (!httpServer.listening) {
+        listeningCallback = cb;
+    } else {
+        cb();
+    }
+};
--- src/database/index.js
+++ src/database/index.js
@@ -1,10 +1,10 @@
 var knex = require('knex')({
     client: 'mysql',
     connection: {
-        host: 'localhost',
-        user: 'admin',
-        password: '',
-        database: 'confisped'
+        host: process.env.RDS_HOSTNAME || 'localhost',
+        user: process.env.RDS_USERNAME || 'admin',
+        password: process.env.RDS_PASSWORD || '',
+        database: process.env.RDS_DB_NAME || 'confisped'
     }
 });
 module.exports = knex
